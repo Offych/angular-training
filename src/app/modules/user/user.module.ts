8581d8ba293d7ab8ddcee3041619f3af21f6e582
Alexander Smotrikov <asmotrikov@fbsdata.com>
2021-03-03 14:05:30 +0300

    Two wau binding, private/public modificators example

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { UserItemComponent } from './components/user-item/user-item.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [AddUserComponent, UserItemComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     AddUserComponent,
